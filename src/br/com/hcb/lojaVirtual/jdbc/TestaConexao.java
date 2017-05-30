ec5008c3caccd441125a4dfe52ce855caed379a3
Hanna Barbosa <hbarbosa@dxc.com>
2017-05-30 08:32:28 -0300

    conexao jdbc
    teste jdbc e mysql

---
+++ src/br/com/hcb/lojaVirtual/jdbc/TestaConexao.java
@@ -0,0 +1,31 @@
+package br.com.hcb.lojaVirtual.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaConexao {
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/lojaVirtual", "root", "1234");
+        
+        Statement statement = connection.createStatement();
+        boolean resultado = statement.execute("select * from produto");
+        System.out.println("É possível retornar resultado dessa consulta: "+ resultado);
+        
+        System.out.println("Resultado:");
+        ResultSet resultSet = statement.getResultSet();        
+        
+        while(resultSet.next()){
+            System.out.println("Produto: " + resultSet.getString("descricao") + ", preco: " + resultSet.getDouble("preco"));
+        }      
+        
+        resultSet.close();
+        statement.close();
+        
+        connection.close();
+    }
+
+}
